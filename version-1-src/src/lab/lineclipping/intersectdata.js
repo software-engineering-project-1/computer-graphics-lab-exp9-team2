b5fc51485b2f5a6f4c340deaf17f7d00346a66f9
Guntireddy <gvijay@qti.qualcomm.com>
2016-02-21 22:20:19 +0530

    finalsubmission : removed unwanted files

--- version-1-src/src/lab/lineclipping/intersectdata.js
+++
@@ -1,7 +0,0 @@
- var intersectdata = function(start1,end1,type)
-      {
-          this.start = start1.slice();
-          this.end = end1.slice();
-          this.pointtype = type;
-          
-      };
